38fb9c06e51d208d771c57f40851504a750760f9
blap <baudouinlapeze9@gmail.com>
2024-10-17 14:13:49 +0200

    exo 2

--- Exo2/Exo/Source/TrainingCPP/InGameWidget.cpp
+++ Exo2/Exo/Source/TrainingCPP/InGameWidget.cpp
@@ -3,24 +3,3 @@
 
 #include "InGameWidget.h"
 
-void UInGameWidget::OnButtonRedClicked(bool bIsClicked)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Button Red Clicked"));
-}
-
-void UInGameWidget::OnButtonBlueClicked(bool bIsClicked)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Button Blue Clicked"));
-}
-
-void UInGameWidget::NativeConstruct()
-{
-	Super::NativeConstruct();
-
-	OnButtonClickedDelegate.AddDynamic(this, &UInGameWidget::OnButtonRedClicked);
-}
-
-void UInGameWidget::OnButtonClicked()
-{
-	OnButtonClickedDelegate.Broadcast(true);
-}
